d09a868e70c21b4843663cf34f731495cdd2379d
AlessandroPucci <a_pucci@hotmail.it>
2017-03-31 20:19:07 +0200

    Creato lo sparo della granata

---
+++ Esercizi_2017_03_30/SurvivalShooter/Assets/Scripts/Player/PlayerGranade.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGranade : MonoBehaviour {
+
+	public int damagePerShot = 40;
+	public float timeBetweenGranades = 1f;
+	public float throwForce = 6f;
+	public float explosionRadius = 10f;
+	public GameObject granade;
+	public Rigidbody playerRB;
+
+	float timer;
+	int shootableMask;
+	int floorMask;
+
+	// Use this for initialization
+	void Awake()
+	{
+		shootableMask = LayerMask.GetMask ("Shootable");
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+
+		if(Input.GetButton ("Fire2") && timer >= timeBetweenGranades && Time.timeScale != 0)
+		{
+			Throw ();
+		}
+	}
+		
+
+	void Throw()
+	{
+		timer = 0f;
+		GameObject granadeInstance = Instantiate (granade, this.transform.position, this.transform.rotation);
+		granadeInstance.AddComponent<Rigidbody> ();
+		Rigidbody granadeRB = granadeInstance.GetComponent <Rigidbody> ();
+
+		granadeRB.AddForce (playerRB.transform.forward * throwForce, ForceMode.Impulse);
+		granadeRB.AddForce (playerRB.transform.up * throwForce, ForceMode.Impulse);
+	}
+		
+}
