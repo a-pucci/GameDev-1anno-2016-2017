dc21cf88a54a784fa3c4f64c5517f4a616299d1c
AlessandroPucci <a_pucci@hotmail.it>
2017-04-19 18:27:56 +0200

    no message

---
+++ Unity/RogueLike/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	public static GameManager Instance = null;
+
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playerTurn = true;
+
+
+	private BoardManager _boardManager;
+	private int _level = 3;
+
+	void Awake()
+	{
+		// Singleton Pattern - Start
+		if(Instance == null)
+		{
+			Instance = this;
+		}
+		else if(Instance != this)
+		{
+			Destroy (gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+		// Singleton Pattern - End
+
+		_boardManager = GetComponent <BoardManager> ();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.InitGame ();
+	}
+	
+	// Update is called once per frame
+	void InitGame () 
+	{
+		_boardManager.SetupScene (_level);
+	}
+
+	public void GameOver()
+	{
+		this.enabled = true;
+	}
+}
---
+++ Unity/RogueLike/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject 
+{
+	public int wallDamage = 1;
+	public int pointsPerFood = 10;
+	public int pointsPerSoda = 20;
+	public float restartLevelDelay = 1f;
+
+	private Animator _animator;
+	private int _food;
+
+	// Use this for initialization
+	protected override void Start () 
+	{
+		_animator = GetComponent<Animator> ();
+		_food = GameManager.Instance.playerFoodPoints;
+
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
