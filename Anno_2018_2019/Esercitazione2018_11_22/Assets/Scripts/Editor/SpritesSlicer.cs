ca0956d333d2f75c92ac272e70bff97d8107f29d
Unknown <a_pucci@hotmail.it>
2018-11-26 18:22:00 +0100

    Custom Slicer wip

--- Anno_2018_2019/Esercitazione2018_11_22/Assets/Scripts/Editor/SpritesSlicer.cs
+++ Anno_2018_2019/Esercitazione2018_11_22/Assets/Scripts/Editor/SpritesSlicer.cs
@@ -6,28 +6,44 @@ using Sirenix.OdinInspector.Editor;
 
 public class SpritesSlicer : OdinEditorWindow
 {
-	[PropertyOrder(-4)]
-	public int sliceWidth ;
-	[PropertyOrder(-3)]
-	public int sliceHeight;
+	[FoldoutGroup("Slice By Cell Size")][PropertyOrder(-4)]
+	public int cellWidth ;
+	[FoldoutGroup("Slice By Cell Size")][PropertyOrder(-4)]
+	public int cellHeight;
+
+	[FoldoutGroup("Slice By Cell Count")] [PropertyOrder(-3)]
+	public int rowsCount;
+	[FoldoutGroup("Slice By Cell Count")] [PropertyOrder(-3)]
+	public int columnsCount;
+
+	//[FoldoutGroup("Slice Settings")][PropertyOrder(-5)] 
+	//public Vector2 pivot = new Vector2(0.5f, 0.5f);
+	[FoldoutGroup("Slice Settings")] [PropertyOrder(-5)]
+	public PivotPositions pivot;
+	[FoldoutGroup("Slice Settings")] [PropertyOrder(-5)]
+	public int widthSpace;
+	[FoldoutGroup("Slice Settings")] [PropertyOrder(-5)]
+	public int heightSpace;
+	
 	[Space(20)]
+	[PropertyOrder(10)]
 	public List<Texture2D> textures;
 	
 	[MenuItem("Tools/Slice Sprites")]
-	static void OpenWindow()
+	private static void OpenWindow()
 	{
 		GetWindow<SpritesSlicer>().Show();
 	}
 	
 	[PropertyOrder(-1)]
-	[ButtonGroup]
+	[FoldoutGroup("Custom Sprite Slicers")]
 	[Button(ButtonSizes.Medium)]
-	void SlicePokemonSprites()
+	private void SlicePokemonSprites()
 	{
 		for (int z = 0; z < textures.Count; z++)
 		{
 			string path = AssetDatabase.GetAssetPath(textures[z]);
-			TextureImporter ti = AssetImporter.GetAtPath(path) as TextureImporter;
+			var ti = AssetImporter.GetAtPath(path) as TextureImporter;
 			ti.isReadable = true;
 			ti.spriteImportMode = SpriteImportMode.Multiple;
  
@@ -64,29 +80,30 @@ public class SpritesSlicer : OdinEditorWindow
 		Debug.Log("Done Slicing!");
 	}
 	
-	[PropertyOrder(-1)]
-	[ButtonGroup]
+	[PropertySpace(10)]
+	[PropertyOrder(-4)]
+	[FoldoutGroup("Slice By Cell Size")]
 	[Button(ButtonSizes.Medium)]
-	void SliceSprites()
+	private void SliceSpritesByCellSize()
 	{
 		for (int z = 0; z < textures.Count; z++)
 		{
 			string path = AssetDatabase.GetAssetPath(textures[z]);
-			TextureImporter ti = AssetImporter.GetAtPath(path) as TextureImporter;
+			var ti = AssetImporter.GetAtPath(path) as TextureImporter;
 			ti.isReadable = true;
 			ti.spriteImportMode = SpriteImportMode.Multiple;
  
 			var newData = new List<SpriteMetaData>();
 			int count = 0;
-			for (int i = 0; i < textures[z].width; i += sliceWidth)
+			for (int i = 0; i < textures[z].width; i += cellWidth + widthSpace)
 			{
-				for (int j = textures[z].height; j > 0; j -= sliceHeight)
+				for (int j = textures[z].height; j > 0; j -= cellHeight + heightSpace)
 				{
-					SpriteMetaData smd = new SpriteMetaData();
-					smd.pivot = new Vector2(0.5f, 0.5f);
+					var smd = new SpriteMetaData();
+					smd.pivot = GetPivotPosition(pivot);
 					smd.alignment = 9;
 					smd.name = textures[z].name + "_" + count;
-					smd.rect = new Rect(i, j - sliceHeight, sliceWidth, sliceHeight);
+					smd.rect = new Rect(i, j - cellHeight, cellWidth, cellHeight);
 			
 					newData.Add(smd);
 				}
@@ -97,5 +114,88 @@ public class SpritesSlicer : OdinEditorWindow
 			AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
 		}
 		Debug.Log("Done Slicing!");
+	}
+	
+	[PropertySpace(10)]
+	[PropertyOrder(-3)]
+	[FoldoutGroup("Slice By Cell Count")]
+	[Button(ButtonSizes.Medium)]
+	private void SliceSpritesByCellsCount()
+	{
+		for (int z = 0; z < textures.Count; z++)
+		{
+			string path = AssetDatabase.GetAssetPath(textures[z]);
+			var ti = AssetImporter.GetAtPath(path) as TextureImporter;
+			ti.isReadable = true;
+			ti.spriteImportMode = SpriteImportMode.Multiple;
+
+			int newCellHeight = textures[z].height / columnsCount;
+			int newCellWidth = textures[z].width / rowsCount;
+ 
+			var newData = new List<SpriteMetaData>();
+			int count = 0;
+			for (int i = 0; i < rowsCount; i += newCellHeight + widthSpace)
+			{
+				for (int j = columnsCount; j > 0; j -= newCellWidth + heightSpace)
+				{
+					var smd = new SpriteMetaData();
+					smd.pivot = GetPivotPosition(pivot);
+					smd.alignment = 9;
+					smd.name = textures[z].name + "_" + count;
+					smd.rect = new Rect(i, j - newCellHeight, newCellWidth, newCellHeight);
+			
+					newData.Add(smd);
+				}
+				count++;
+			}
+			
+			ti.spritesheet = newData.ToArray();
+			AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+		}
+		Debug.Log("Done Slicing!");
+	}
+
+	private Vector2 GetPivotPosition(PivotPositions pivotPosition)
+	{
+		var pivotPos = new Vector2();
+		
+		switch (pivotPosition)
+		{
+			case PivotPositions.Center:
+				pivotPos = new Vector2(0.5f, 0.5f);
+				break;
+				
+			case PivotPositions.TopLeft:
+				pivotPos = new Vector2(0.5f, 0.5f);
+				break;
+			
+			case PivotPositions.TopRight:
+				pivotPos = new Vector2(0.5f, 0.5f);
+				break;
+			
+			case PivotPositions.BottomLeft:
+				pivotPos = new Vector2(0.5f, 0.5f);
+				break;
+			
+			case PivotPositions.BottomRight:
+				pivotPos = new Vector2(0.5f, 0.5f);
+				break;
+				
+		}
+		
+		return pivotPos;
+	}
+	
+	public enum PivotPositions
+	{
+		TopLeft,
+		TopCenter,
+		TopRight,
+		CenterLeft,
+		Center,
+		CenterRight,
+		BottomLeft,
+		BottomCenter,
+		BottomRight
 	}
 }
